d0be437d1281223aeed2cedc6851af9efb2e226f
Crowdbotics <team@crowdbotics.com>
2020-12-18 19:58:25 +0000

    add package blueprint-react-native-sign-in-1-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'SignIn11186620', human_name: 'Sign In 1 1', access_route: 'SignIn11186620'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn11186620Navigator from '../features/SignIn11186620/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn11186620: { screen: SignIn11186620Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
